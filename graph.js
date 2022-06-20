0e9fbb70ad4c5efad2ee5ffcd626d665d0f34b27
minoli-g <minoliniroshani@gmail.com>
2022-06-20 23:40:40 +0530

    Commit hardcode definitions of graph

---
+++ graph.js
@@ -0,0 +1,11 @@
+let graph3 = new Map();
+
+graph3.set(1, [2,3,4]);
+graph3.set(2, [1,3,4,5,6]);
+graph3.set(3, [2,5,6]);
+graph3.set(4, [1,2,5,7,8]);
+graph3.set(5, [1,2,3,4,6,7,8,9]);
+graph3.set(6, [2,3,5,8,9]);
+graph3.set(7, [4,5,8]);
+graph3.set(8, [4,5,6,7,9]);
+graph3.set(9, [5,6,8]);
