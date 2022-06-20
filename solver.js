90695345197656a4bc3a65361850c426c1492be4
minoli-g <minoliniroshani@gmail.com>
2022-06-20 12:31:01 +0530

    Add Board constructor

--- solver.js
+++ solver.js
@@ -43,3 +43,56 @@ class TrieNode {
         }
     }
 }
+
+class Board {
+
+    constructor(boardStr){
+        let arr = boardStr.split("-");
+        
+        this.board = [];
+        for (let item of arr){
+            this.board.push(item.split(""));
+        }
+
+        this.size = arr.length;
+        this.nodes = new Map();
+        this.graph = new Map();
+
+        for (let i=0; i<this.size; i++){
+            for (let j=0; j<this.size; j++){
+
+                this.nodes.set( [i,j], this.board[i][j]);
+
+                let neighbours=[];
+
+                if (i>0) {
+                    neighbours.push( [i-1,j] );  // North
+                    if (j>0){
+                        neighbours.push( [i-1, j-1] )  // North-west
+                    }
+                    if (j<this.size-1){
+                        neighbours.push( [i-1, j+1] )  // North-east
+                    }
+                }
+                if (j>0){
+                    neighbours.push( [i, j-1] ); // West
+                }
+                if (i<this.size-1){
+                    neighbours.push( [i+1, j] ); // East
+                    if (j>0){
+                        neighbours.push( [i+1, j-1] ); // South-west
+                    }
+                    if (j<this.size-1){
+                        neighbours.push( [i+1, j+1] ); // South-east
+                    }
+                }
+                if (j<this.size-1){
+                    neighbours.push( [i, j+1] ); // South
+                }
+
+                this.graph.set( [i,j], neighbours);
+            }
+        }
+        console.log(this.graph);
+    }
+}
