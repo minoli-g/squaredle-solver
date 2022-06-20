d60fd710843ee97ea19d25a94b2dac7099f97fe4
minoli-g <minoliniroshani@gmail.com>
2022-06-20 16:37:23 +0530

    Removed unnecessary logic of Trie

--- solver.js
+++ solver.js
@@ -5,7 +5,7 @@ function add(a,b){
 class TrieNode {
     
     constructor(){
-        this.children = new Map(); //Map the letter int code to a new TrieNode
+        this.children = new Map(); //Map the letter to a new TrieNode
         this.eow = false; // Boolean to indicate if this is the last letter
     }
     
@@ -13,32 +13,14 @@ class TrieNode {
         
         let curr = this;        
         for (let i=0; i<word.length; i++){
-            
-            var letterCode = word.charCodeAt(i)-97;
-            
-            if ( curr.children.get(letterCode) == null){
-                curr.children.set(letterCode, new TrieNode());
+            let letter = word[i];
+
+            if ( curr.children.get(letter) == null){
+                curr.children.set(letter, new TrieNode());
             }
-            curr = curr.children.get(letterCode);
+            curr = curr.children.get(letter);
             if (i == word.length-1){
                 curr.eow = true;
-            }
-        }
-    }
-    
-    findEarliestWords(wordSoFar, wordsCollected){
-        
-        if (wordsCollected.length < 3){
-            
-            if (this.eow){
-                wordsCollected.push(wordSoFar);
-            }
-            for (let i=0; i<26; i++){
-                
-                if (this.children.get(i) != null){
-                    let updatedWord = wordSoFar + String.fromCharCode(97+i);
-                    this.children.get(i).findEarliestWords(updatedWord, wordsCollected);
-                }
             }
         }
     }
